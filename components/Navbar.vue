7abc1f149ea1c52e50ddb38785b30cd77c3dee08
Thaekeh <thaekeh@gmail.com>
2020-11-01 23:08:28 +0100

    Init

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app-bar fixed app color="#FEFAF6" flat>
+      <v-toolbar-title id="toolbarTitle">Thaeke Hekkenberg</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="hidden-sm-and-down">
+        <v-btn text class="navbarLink" href="#work"> Work </v-btn>
+        <v-btn text class="navbarLink" href="#about"> About Me </v-btn>
+        <v-btn text class="navbarLink" href="#contact"> Contact </v-btn>
+      </span>
+      <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
+    </v-app-bar>
+</template>
+
+<style lang="sass" scoped>
+#toolbarTitle
+  font-family: 'Montserrat', sans-serif
+  font-weight: 500
+
+.navbarLink
+  font-family: 'Montserrat', sans-serif
+  font-weight: 500  
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <navbar></navbar>
+    <v-main class="primary">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
